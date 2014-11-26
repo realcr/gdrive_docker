70c3d6c15249ebe2398ba3ba8790a28f3d15083e
real <real@freedomlayer.org>
2014-11-26 15:52:42 +0200

    Fixed bash syntax and allow upload while creating directories (Recursive).

--- store_file.sh
+++ store_file.sh
@@ -20,27 +20,28 @@ fi
 
 # If the cred_image_cont does not exist, we create it:
 nlines_cred=`docker ps -a | grep gdrive_cred_cont | wc -l`
-if [ "$nlines_cred" -eq "0" ] then 
+if [ "$nlines_cred" -eq "0" ]; then 
 	echo "gdrive_cred_cont doesn't exist. Aborting."
-	exit
+	exit;
 fi
 
 nlines_core=`docker ps -a | grep gdrive_core_cont | wc -l`
-if [ "$nlines_core" -gt "0" ] then 
+if [ "$nlines_core" -gt "0" ]; then 
 	echo "gdrive_core_cont still exists. We remove it first."
 	docker rm -f gdrive_core_cont
 fi
 
 # The destination path of the file inside the container:
-container_dest=/root/gdrive/${DEST_DIR}/$(basename $FILE_TO_STORE)
+container_dest_file=/root/gdrive/${DEST_DIR}/$(basename $FILE_TO_STORE)
+container_dest_dir=/root/gdrive/${DEST_DIR}/
 
 # Interactive script to generate credentials:
 # (User has to open his browser to get the code back)
 docker run -it --name gdrive_core_cont \
-	-v ${FILE_TO_STORE}:${container_dest} \
+	-v $(readlink -f $FILE_TO_STORE):${container_dest_file} \
 	--volumes-from gdrive_cred_cont \
 	gdrive_core_image \
-	/root/gocode/bin/drive push -no-prompt ${container_dest}
+	/root/gocode/bin/drive push -no-prompt -r ${container_dest_dir}
 
 # Remove the gdrive_core_cont container:
 docker rm -f gdrive_core_cont
